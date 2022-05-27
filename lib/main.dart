f3e213ddda66053f337f5cfdaaa51f674fa714e6
João Victor Terzi <jv_ermel@hotmail.com>
2022-05-27 14:30:01 -0300

    Inserindo a funcionalidade de alterar entre gráfico e lista na AppBar somente quando em modo Paisagem

--- lib/main.dart
+++ lib/main.dart
@@ -92,13 +92,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: const Text('Despesas Pessoais'),
       actions: [
+        if (isLandscape)
+          IconButton(
+            onPressed: () {
+              setState(
+                    () {
+                  _showChart = !_showChart;
+                },
+              );
+            },
+            icon: Icon(_showChart ? Icons.list : Icons.show_chart),
+          ),
         IconButton(
           onPressed: () => _openTransactionFormModal(context),
           icon: const Icon(Icons.add),
         ),
+
       ],
     );
 
@@ -112,29 +127,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Exibir o Gráfico: '),
-                Switch(
-                  value: _showChart,
-                  onChanged: (value) {
-                    setState(() {
-                      _showChart = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            if (_showChart)
+            if (_showChart || !isLandscape)
               SizedBox(
-                height: availableHeight * 0.3,
+                height: availableHeight * (isLandscape ? 0.7 : 0.3),
                 child: Chart(_recentTransactions),
               ),
             const SizedBox(
               height: 16,
             ),
-            if (!_showChart)
+            if (!_showChart || !isLandscape)
               SizedBox(
                 height: availableHeight * 0.7,
                 child: TransactionList(_transactions, _removeTransaction),
